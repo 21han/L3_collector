522ddea38880fd6470d799219c4e5b6cd110445a
Zihan Guo <zg2391@columbia.edu>
2021-05-19 23:58:16 -0400

    init dev

--- README.md
+++ README.md
@@ -1,1 +1,123 @@
 # L3_collector
+
+## Background
+
+- Tick data is the highest resolution data.
+- sequence of executed trade + bid/ask quote
+
+## Difference between L2 and L3 data
+
+> L3 data (market-by-order) includes ever bid and ask added, updated, or removed from an exchange's order book, non-aggregated.
+Normally, order books are aggregated by price level to include all bids or asks placed at a given price level. 
+However, it is impossible to determine whether these bids/asks are from many traders or just 1. 
+L3 data allows you to see individual bids/asks added to an order book. 
+L2 (market-by-price-level) data includes these bids and asks aggregated by price level. 
+> Only 3 exchanges in the industry provide non-aggregated, L3 data. All other exchanges provide L2 order boook data. [2]
+
+
+## Types of Tick Data
+
+There are 4 types of tick data. 
+
+- trade tick data (sequence of executed trade)
+- Level 1: Best Bid/Offer or BBO quotes
+- Level 2: partial/full order book
+- Level 3: execution and amendment actions (e.g. update a limit order or cancel a limit order)
+
+Sometimes full order book is also referred as L3 data 
+as long as it is not aggregated at a price level.
+
+## Motivation
+
+- improve backtesting and build simulation using more realistic model execution price based on order book
+- better understanding of institutional and retail trade behavior
+- arbitrage opportunities
+- empower more advanced signal construction/strategies
+
+
+## Coverage
+
+For this project, we will cover `Binance` and `Coinbase` L3 data collection.
+
+As of today (2021-05-16) only 3 crypto exchanges offer L3 data feed: Coinbase, Bitstamp, and Bitfinex. Therefore, for
+`Binance`, we will collect the following data feed:
+
+* trade tick data (sequence of executed trade)
+* the most recent orderbook (depth = 5) (extensible to more depth)
+
+We want to mange the local order book. In order to do so, we followed the [3] guide from Binance
+official documentation. Conveniently, the described algorithm has already been implemented by
+[_process_depth_message](https://python-binance.readthedocs.io/en/latest/_modules/binance/depthcache.html)
+method in `DepthCacheManager` class. Therefore, we avoid re-inventing wheel and use this method directly
+to log/store the most recent order book (depth = 5).
+
+In addition, according to binance official doc, the websocket (@depth) offers two update speed:
+
+- 1000 ms (1 second)
+- 100 ms (0.1 second)
+
+We will default to 1000 ms, but can easily change this by updating the `config.json` file.
+
+
+## Argument Options
+
+User needs to pass in `exchange`, `symbol`, `data_type`, `data_dir` to the python program.
+
+Possible options are specified below:
+
+```python
+parser.add_argument("--exchange", required=True, choices=["coinbase", "binance"])
+parser.add_argument("--symbol", required=True, type=str, help="Format: {base asset}{quote asset} (ex. 'BTCUSDT')")
+parser.add_argument("--data_dir", type=str, help="local data directory path", default="./data/")
+```
+
+
+## Data Quality Check
+
+The data quality check is a completely independent process from the scraper. The advantage of this set-up is such that 
+data quality check has zero interference with the scraper. (i.e. if data quality check process fails, it will not affect
+the scraper or data collector in any ways and thereby adding robustness to the pipeline). In addition, we have added
+slack integration (and PagerDuty possibly in the future) to further enhance any data quality issues we catch.
+
+
+## ETL Workflow
+
+- TODO: add in draw.io chart
+
+## Failure Prevention
+
+### Network Level
+
+At network level, we can set up concurrent processes in three geographical region: Asia, Europe and America. 
+For example, we run three separate AWS ec2 instances in (1) Dublin, Europe; (2) Ohio, USA; (3) Tokyo, Japan to reduce 
+the probability that we experience any service outage in one single geographical region.
+
+The probabilities that all three regions have network failure is marginal. 
+
+To even further enhance failure prevention measure, we can run concurrent process using GCP, Azure along with AWS to prevent 
+enterprise level network failure instead of geography specific failure. 
+
+### API Level
+
+At API Level, exchange could change their APIs. However, this is already handled by the code logic by always saving the 
+raw message and persist to S3. Therefore, any API changes are completely recoverable and interrupted data can be 
+back-filled easily. 
+
+In addition, we can set up proper alerts that subscribe to exchange API updates on Slack; such that, we are aware of any
+API change plans ahead of time.
+
+## Security Master
+
+We can design the security master as the following:
+
+
+## Readings
+
+[1] [tick data](https://firstratedata.com/a/4/tick-data-intro)
+
+[2] [Kaiko](https://www.kaiko.com/collections/order-books/binance)
+
+[3] [Binance Official Guide on Managing Local Orderbook Correctly](https://github.com/binance/binance-spot-api-docs/blob/master/web-socket-streams.md#all-market-tickers-stream)
+
+
+
---
+++ src/collect.py
@@ -0,0 +1,302 @@
+import argparse
+import asyncio
+from loguru import logger
+import time
+import datetime
+
+
+def set_logger(data_dir: str):
+    """
+    set logging behavior, and handles compression, rotation automatically
+    :param data_dir:
+    :param ticker:
+    :param ex:
+    :return:
+    """
+    logger.remove()
+    data_prefix = f"{data_dir}{exchange_name}/{symbol}/{data_type}"
+    logger.add(
+        f"{data_prefix}/parsed/{{time}}.csv",
+        format="{message}",
+        rotation="256 MB",
+        compression="gz",
+        filter=lambda record: "task" in record["extra"] and record["extra"]["task"] == task_name
+    )
+    logger.add(
+        f"{data_prefix}/raw/{{time}}.txt",
+        format="{message}",
+        rotation="256 MB",
+        compression="gz",
+        filter=lambda record: "task" in record["extra"] and record["extra"]["task"] == f"{task_name}_raw"
+    )
+
+
+async def binance_tick_trade_data():
+    """
+    collect binance_tick_trade_data
+    :return:
+    """
+    from binance import AsyncClient, BinanceSocketManager
+
+    raw_data_logger = logger.bind(task=f"{task_name}_raw")
+    data_logger = logger.bind(task=task_name)
+
+    def process_binance_trade_socket_msg(msg):
+        """
+        parser and logger helper specific to binance tick trade data
+        :param msg:
+        :return:
+        """
+        raw_data_logger.info(msg)
+        # e: trade, E: event time, s: symbol, t: Trade ID, p: Price, q: Quantity, T: trade time, m: mm or not
+        # TODO add try and catch logic here
+
+        # note: difference between event time and trade time
+        # https://stackoverflow.com/questions/50688471/binance-event-time-vs-trade-time
+
+        parsed_msg = f"{msg['E']},{msg['s']},{msg['t']},{msg['p']},{msg['q']}," \
+                     f"{msg['T']},{msg['m']}"
+
+        data_logger.info(parsed_msg)
+
+    client = await AsyncClient.create()
+    bsm = BinanceSocketManager(client)
+
+    async with bsm.trade_socket(symbol) as socket:
+        while True:
+            res = await socket.recv()
+            process_binance_trade_socket_msg(res)
+
+
+async def binance_order_book_snapshot():
+    """
+    get binance snapshot of order book
+    :return:
+    """
+    from binance import AsyncClient, DepthCacheManager
+
+    client = await AsyncClient.create()
+    dcm = DepthCacheManager(client, symbol)
+    raw_data_logger = logger.bind(task=f"{task_name}_raw")
+    data_logger = logger.bind(task=task_name)
+    depth = 5  # TODO add this to config.json
+
+    async with dcm as dcm_socket:
+        while True:
+            depth_cache = await dcm_socket.recv()
+            update_time = depth_cache.update_time
+            depth_symbol = depth_cache.symbol
+            bids = depth_cache.get_bids()[:depth]
+            asks = depth_cache.get_asks()[:depth]
+            msg = {
+                'update_time': update_time,
+                'symbol': depth_symbol,
+                'bids': bids,
+                'asks': asks,
+                'depth': depth
+            }
+            raw_data_logger.info(msg)
+            # TODO: add try and catch logic here
+            parsed_data = ""
+            for i in range(depth):
+                bid_msg = f"{update_time}," \
+                          f"{symbol}," \
+                          f"B," \
+                          f"{bids[i][0]}," \
+                          f"{bids[i][1]}," \
+                          f"{i + 1}"
+                ask_msg = f"{update_time}," \
+                          f"{symbol}," \
+                          f"A," \
+                          f"{asks[i][0]}," \
+                          f"{asks[i][1]}," \
+                          f"{i + 1}"
+                parsed_data += f"\n{bid_msg}\n{ask_msg}"
+
+            data_logger.info(parsed_data.strip())
+
+
+async def collect_binance():
+    """
+    citation: https://python-binance.readthedocs.io/en/latest/websockets.html#binancesocketmanager-websocket-usage
+    process all tick data using async client for a particular symbol. detailed can be found in the link above
+    :return:
+    """
+    if data_type == "trade":
+        await binance_tick_trade_data()
+    elif data_type == "book":
+        await binance_order_book_snapshot()
+    else:
+        raise ValueError(f"invalid data_type: {data_type}")
+
+
+def unix_time_millis(dt):
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    return round((dt - epoch).total_seconds() * 1000.0)
+
+
+def parse_z_str_to_dt(s):
+    return datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+
+def coinbase_tick_trade_data():
+    """
+    get coinbase tick trade data (orders that have been matched and taken off the order book)
+    future enhacement note: we can add heartbeat channel to track the last trade id
+    and fetch trade that we potentially miss from the REST API in concurrent with this process.
+
+    link: https://docs.pro.coinbase.com/#the-matches-channel
+
+    :return:
+    """
+    from copra.websocket import Channel, Client
+    raw_data_logger = logger.bind(task=f"{task_name}_raw")
+    data_logger = logger.bind(task=task_name)
+
+    class CoinbaseClient(Client):
+        def on_message(self, msg):
+            if msg['type'] == 'match':
+                raw_data_logger.info(msg)
+                trade_time = parse_z_str_to_dt(msg['time'])
+                trade_time_in_millis = unix_time_millis(trade_time)
+                data_logger.info(f"{round(time.time() * 1000)},"  # socket time
+                                 f"{msg['product_id']},"  # e.g. BTC-USD
+                                 f"{msg['trade_id']},"
+                                 f"{msg['price']},"
+                                 f"{msg['size']},"  # quantity
+                                 f"{trade_time_in_millis},"
+                                 f"{True if msg['side'] == 'buy' else False}")  # is the Buyer the market maker?
+
+    channel = Channel('matches', symbol)
+    ws = CoinbaseClient(loop, channel)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        loop.run_until_complete(ws.close())
+
+
+def coinbase_order_book_snapshot():
+    from copra.websocket import Channel, Client
+    raw_data_logger = logger.bind(task=f"{task_name}_raw")
+    data_logger = logger.bind(task=task_name)
+
+    class OrderBookWebSocketClient(Client):
+
+        def __init__(self, loop, channel):
+            super().__init__(loop, channel)
+            self.depth_cache = {
+                'A': [],
+                'B': []
+            }
+            self.depth = 5  # subject to change
+            self.frequency = 1000  # in milliseconds
+            self.elastic_ratio = 3  # to avoid vanishing boundary problem
+
+        def on_message(self, msg):
+            if "type" in msg and msg['type'] == "l2update":
+                raw_data_logger.info(msg)
+
+                # parse L2 updates
+                change = msg['changes']
+                if change[0][0] == 'buy':
+                    # dedup
+                    self.depth_cache['B'] = [i for i in self.depth_cache['B'] if i[0] != float(change[0][1])]
+                    self.depth_cache['B'].append(
+                        (
+                            float(change[0][1]),  # price
+                            float(change[0][2])   # quantity
+                        )
+                    )
+                elif change[0][0] == 'sell':
+                    # dedup
+                    self.depth_cache['A'] = [i for i in self.depth_cache['A'] if i[0] != float(change[0][1])]
+                    self.depth_cache['A'].append(
+                        (
+                            float(change[0][1]),  # price
+                            float(change[0][2])   # quantity
+                        )
+                    )
+                else:
+                    logger.error(f"invalid record: {msg}")
+
+                self.depth_cache['A'] = [r for r in self.depth_cache['A'] if r[1] != 0]
+                self.depth_cache['A'].sort(key=lambda r: r[0], reverse=False)
+                self.depth_cache['A'] = self.depth_cache['A'][:self.depth*self.elastic_ratio]
+
+                self.depth_cache['B'] = [r for r in self.depth_cache['B'] if r[1] != 0]
+                self.depth_cache['B'].sort(key=lambda r: r[0], reverse=True)
+                self.depth_cache['B'] = self.depth_cache['A'][:self.depth*self.elastic_ratio]
+
+                msg_dt = parse_z_str_to_dt(msg['time'])
+                msg_mili = unix_time_millis(msg_dt)
+
+                if len(self.depth_cache['A']) >= 5 and len(self.depth_cache['B']) >= 5 \
+                        and msg_mili % 1000 == 0:
+                    parsed_data = ""
+                    for i in range(self.depth):
+                        bid_msg = f"{msg_mili}," \
+                                  f"{msg['product_id']}," \
+                                  f"B," \
+                                  f"{self.depth_cache['B'][i][0]}," \
+                                  f"{self.depth_cache['B'][i][1]}," \
+                                  f"{i + 1}"
+                        ask_msg = f"{msg_mili}," \
+                                  f"{msg['product_id']}," \
+                                  f"A," \
+                                  f"{self.depth_cache['A'][i][0]}," \
+                                  f"{self.depth_cache['A'][i][1]}," \
+                                  f"{i + 1}"
+                        parsed_data += f"\n{bid_msg}\n{ask_msg}"
+
+                    data_logger.info(parsed_data.strip())
+
+    # channel description: https://docs.pro.coinbase.com/#the-level2-channel
+    channel = Channel('level2', symbol)
+    ws = OrderBookWebSocketClient(loop, channel)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        loop.run_until_complete(ws.close())
+
+
+def collect_coinbase():
+    if data_type == "trade":
+        coinbase_tick_trade_data()
+    elif data_type == "book":
+        coinbase_order_book_snapshot()
+    else:
+        raise ValueError(f"invalid data_type: {data_type}")
+
+
+if __name__ == "__main__":
+    """
+    e.g.
+    for binance exchange, we want to collect "BTCUSDT", and we want the data to be order book data with depth = 5,
+    and we want to collect the data with either 100 ms or 1000 ms refresh rate. we want to save the data in ...
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--exchange", required=True, choices=["coinbase", "binance"])
+    parser.add_argument("--symbol", required=True, type=str, help="Format: {base asset}{quote asset} (E.g 'BTCUSDT')")
+    parser.add_argument("--data_type", required=True, choices=["trade", "book"],
+                        help="tick data data or order book data")
+    parser.add_argument("--data_dir", type=str, help="root local data directory", default="./data/")
+
+    args = parser.parse_args()
+
+    symbol = args.symbol.upper()
+
+    exchange_name = args.exchange.lower()
+    data_type = args.data_type.lower()
+
+    task_name = f"{exchange_name}_{symbol}_{data_type}"
+    logger.debug(f'Collecting {exchange_name} {symbol} Tick {data_type} Data')
+    set_logger(args.data_dir)
+    loop = asyncio.get_event_loop()
+    if exchange_name == "binance":
+        loop.run_until_complete(collect_binance())
+    elif exchange_name == "coinbase":
+        collect_coinbase()
+
+    # ^ [binance and coinbase] add more exchanges in the future
